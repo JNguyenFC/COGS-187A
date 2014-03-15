f8d519bb33fa2fb96bd62f8579b18423d239a08c
JeffNguyen <jeffmn93@gmail.com>
2014-03-14 19:24:45 -0700

    gallery+review

--- _COFFEE/js/intro.js
+++ _COFFEE/js/intro.js
@@ -12,3 +12,24 @@ $(document).ready(function(){
 	    });
 	});
 });
+
+$(window).load(function() {
+  // The slider being synced must be initialized first
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  });
+   
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel"
+  });
+});
